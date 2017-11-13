2699987527a83016e9b783088555db1269d3189c
Martirosyan Ruben <mruben93@gmail.com>
2017-11-14 03:02:55 +0400

    Adding Progress View Model

---
+++ WordLearner/ViewModels/ProgressViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordLearner.ViewModels
+{
+    public class ProgressViewModel : BaseModel
+    {
+    }
+}
